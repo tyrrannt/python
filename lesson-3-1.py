6d37f42949ab6286db553a4061b54cd064ecfbe4
Shakirov Vitaliy <shakirov.vitaliy@gmail.com>
2020-04-09 09:40:42 +0300

    Добавлено решение первого примера

--- lesson-3-1.py
+++ lesson-3-1.py
@@ -1,2 +1,21 @@
 # 1. Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление.
 # Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
+
+def division(dividend, divider, rounding=3):
+    """
+    Функция деления двух чисел.
+
+    :param dividend: Параметр в который передается "Делимое", допустимый тип данных int, float.
+    :param divider: Параметр в который передается "Делитель", допустимый тип данных int, float.
+    :param rounding: Необязательный параметр. Округление числа после запятой. По умолчанию 3, допустимый тип данных int.
+    :return: В случае ввода корректных данных возвращает результат деления тип float.
+    """
+    return round(dividend / divider, rounding)
+
+
+try:
+    first_arg = int(input("Введите делимое: "))
+    second_arg = int(input("Введите делитель: "))
+    print(f"{first_arg}/{second_arg} =", division(first_arg, second_arg, 10))
+except Exception as error:
+    print(error)
