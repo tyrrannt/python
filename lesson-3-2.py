3fce215539bf8c4378297941bf14b2021231c832
Shakirov Vitaliy <shakirov.vitaliy@gmail.com>
2020-04-09 16:24:01 +0300

    Добавлено решение третьего примера, но дома надо еще раз посмотреть, и перепроверить

--- lesson-3-2.py
+++ lesson-3-2.py
@@ -2,3 +2,17 @@
 # имя, фамилия, год рождения, город проживания, email, телефон.
 # Функция должна принимать параметры как именованные аргументы.
 # Реализовать вывод данных о пользователе одной строкой.
+
+def personal_data(**kwargs):
+    """
+    Функция для вывода данных пользователя
+
+    :param kwargs: Принимает именованные аргументы: name, surname, birth, city, email, tel
+    :return: Возвращает строковое значение
+    """
+    result = ''
+    for keys, arg in kwargs.items():
+        result += (f"{keys}: {arg}; ")
+    return result
+
+print(personal_data(name='Ivan', age=23, birth='11.10.1981'))
