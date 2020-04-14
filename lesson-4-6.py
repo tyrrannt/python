35172f9b96f0c9adb7cb73870035734558096df3
Shakirov Vitaliy <shakirov.vitaliy@gmail.com>
2020-04-14 11:33:19 +0300

    Сделаны все решения примеров

--- lesson-4-6.py
+++ lesson-4-6.py
@@ -3,15 +3,29 @@
 # б) бесконечный итератор, повторяющий элементы некоторого списка, определенного заранее.
 # Подсказка: использовать функцию count() и cycle() модуля itertools
 
-from itertools import count, cycle
+from itertools import count, cycle, islice
 
 
-def generate(first):
+def generate(first, step=1):
+    """
+    Генерация целых чисел
+    :param first: с какого числа начать
+    :param step: шаг, опциональный аргумент, по умолчании 1
+    :return:
+    возвращает бесконечный итератор
+    """
     for i in count(first, 1):
         yield i
 
 
-for i in generate(10):
-    if i > 150:
+my_list = []
+
+for i in generate(70):
+    if i > 80:
         break
+    # Новый список заполняется символами по их числовому представлению
+    my_list.append(chr(i))
+
+print(my_list)
+for i in islice(cycle(my_list), 12):
     print(i)
