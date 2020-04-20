5a2697b517489e2f22184e4d312a417f189c3110
Shakirov Vitaliy <shakirov.vitaliy@gmail.com>
2020-04-20 18:06:46 +0300

    изменен 1 пример

--- lesson-6-1.py
+++ lesson-6-1.py
@@ -6,7 +6,29 @@
 # Задачу можно усложнить, реализовав проверку порядка режимов, и при его нарушении выводить
 # соответствующее сообщение и завершать скрипт.
 
-from lesson6 import TrafficLight
+from lesson6 import TrafficLight, sleep
 
 traffic = TrafficLight()
-traffic.running()
+
+from tkinter import *
+root = Tk()
+myCanvas = Canvas(root)
+myCanvas.pack()
+
+def create_circle(x, y, r, canvasName, outline, fill): #center coordinates, radius
+    x0 = x - r
+    y0 = y - r
+    x1 = x + r
+    y1 = y + r
+    return canvasName.create_oval(x0, y0, x1, y1, outline=outline, fill=fill)
+
+def clicked(color1='', color2='', color3=''):
+    create_circle(100, 50, 20, myCanvas, 'red', color1)
+    create_circle(100, 100, 20, myCanvas, 'yellow', color2)
+    create_circle(100, 150, 20, myCanvas, 'green', color3)
+
+while 1:
+    myCanvas.update_idletasks()
+    myCanvas.update()
+    sleep(0.01)
+    #clicked()
