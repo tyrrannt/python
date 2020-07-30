4e32daabcf2f8f458e10acf39d56b96a28a5db66
Shakirov Vitaliy <shakirov.vitaliy@gmail.com>
2020-07-30 17:40:16 +0300

    Изменено решение примера №4

--- task_4.py
+++ task_4.py
@@ -9,3 +9,23 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+from random import randrange
+
+def number_sign(num):
+    if num % 2 == 0:
+        return 2 ** num
+    else:
+        return - (2 ** num)
+
+
+def number_of_shifter(num, count):
+    if count == 1:
+        return num[0]
+    else:
+        return num[0] + number_of_shifter(num[1:], count - 1)
+
+
+steps = int(input('Введите количество элементов: '))
+row_numbers = [1 / number_sign(i) for i in range(0, steps)]
+print(row_numbers)
+print(number_of_shifter(row_numbers, steps))
