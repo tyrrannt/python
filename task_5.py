855294b39bd2f98bd740b163d60d426ae479516a
Shakirov Vitaliy <shakirov.vitaliy@gmail.com>
2020-08-09 22:02:56 +0300

    Решен 1, 2, 3 и 4, начат 5

--- task_5.py
+++ task_5.py
@@ -35,24 +35,7 @@ def simple(i):
     return n
 
 def eratosfen(n):
-    a = []
-    b = []
-
-    if n < 4:
-        return
-
-    for i in range(2, n + 1, 1):
-        a.append(i)
-
-    while a:
-        for i in a[1:]:
-            if i % a[0] == 0:
-                b.append(i)
-                a.remove(i)
-        a = a[1:]
-
-    for i in b:
-        print(i, end=" ")
+    
 
 i = int(input('Введите порядковый номер искомого простого числа: '))
 print(simple(i))
