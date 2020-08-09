4bd07ce6adb5ce467b7f09832d23808e90055c89
Shakirov Vitaliy <shakirov.vitaliy@gmail.com>
2020-08-09 18:57:55 +0300

    Решен 1, 2, и 3

--- task_1.py
+++ task_1.py
@@ -20,10 +20,17 @@ def func_1(nums):
             new_arr.append(i)
     return new_arr
 
+# Выполнил в виде генератора списка, почти в 2 раза быстрее
+def func_2(nums):
+    return [i for i in nums if i % 2 == 0]
+
+
 
 if __name__ == '__main__':
     from timeit import Timer
 
-    array = [i for i in range(605)]
+    array = [i for i in range(300)]
     test1 = Timer(lambda: func_1(array))
-    print(test1.timeit(100000))
+    test2 = Timer(lambda: func_2(array))
+    print(test1.timeit(50000))
+    print(test2.timeit(90000))
