26c54582fb137e1085d40b42ed39e561c0e7ce3e
Shakirov Vitaliy <shakirov.vitaliy@gmail.com>
2020-08-09 17:25:13 +0300

    Решен 2, и 3

--- task_2.py
+++ task_2.py
@@ -30,22 +30,32 @@ def memoize(func):
 
 
 @memoize
-def recursive_reverse_mem(number):
+def recursive_reverse_mem(number):  # С мемоизацией самый быстрый вариант
     if number == 0:
         return str(number % 10)
     return f'{str(number % 10)}{recursive_reverse_mem(number // 10)}'
 
 
-def recursive_reverse(number):
+def recursive_reverse(number):  # Самый медленный вариант с рекурсией
     if number == 0:
         return str(number % 10)
     return f'{str(number % 10)}{recursive_reverse(number // 10)}'
 
 
+def reverse(number):  # Средний вариант, обычный разворот
+    origin = str(number)
+    return f'{origin[::-1]}'
+
+
 if __name__ == '__main__':
     from timeit import Timer
 
+    print(
+        f'Мемоизация: {recursive_reverse_mem(65498732110646548)}\nРекурсия: {recursive_reverse(65498732110646548)}\nPазворот: {reverse(65498732110646548)}')
+
     test1 = Timer(lambda: recursive_reverse(65498732110646548))
     test2 = Timer(lambda: recursive_reverse_mem(65498732110646548))
+    test3 = Timer(lambda: reverse(65498732110646548))
+
     print(
-        f'С мемоизацией 10 000 000 запусков = {test2.timeit(number=10000000)}. \nБез мемоизации  330 000  запусков = {test1.timeit(number=330000)}')
+        f'С мемоизацией 10 000 000 запусков = {test2.timeit(number=10000000)}. \nБез мемоизации  330 000  запусков = {test1.timeit(number=330000)}\nОбычный разворот 5 000 000 запусков = {test3.timeit(number=5000000)}')
--- task_3.py
+++ task_3.py
@@ -9,9 +9,13 @@
 
 Сделайте вывод, какая из трех реализаций эффективнее и почему
 """
+import cProfile
+import sys
+
+sys.setrecursionlimit(10000)
 
 
-def revers(enter_num, revers_num=0):
+def revers(enter_num, revers_num=0):  # медленный, за счет рекурсии
     if enter_num == 0:
         return
     else:
@@ -21,7 +25,7 @@ def revers(enter_num, revers_num=0):
         revers(enter_num, revers_num)
 
 
-def revers_2(enter_num, revers_num=0):
+def revers_2(enter_num, revers_num=0):  # средний, цикл отрабатывает быстрее
     while enter_num != 0:
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
@@ -29,14 +33,24 @@ def revers_2(enter_num, revers_num=0):
     return revers_num
 
 
-def revers_3(enter_num):
+def revers_3(enter_num):  # быстрый, так как строка попросту разворачивается
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
 
+
+def main():
+    revers(65498732110646548 ** 100)
+    revers_2(65498732110646548 ** 100)
+    revers_3(65498732110646548 ** 100)
+
+
 if __name__ == '__main__':
     from timeit import Timer
+
     test1 = Timer(lambda: revers(65498732110646548))
     test2 = Timer(lambda: revers_2(65498732110646548))
     test3 = Timer(lambda: revers_3(65498732110646548))
     print(f'revers = {test1.timeit(100000)}; revers_2 = {test2.timeit(100000)}; revers_3 = {test3.timeit(100000)}')
+    cProfile.run('main()')
+
