a250b469f4560f77637899b665514bfc80a47b21
Shakirov Vitaliy <shakirov.vitaliy@gmail.com>
2020-08-20 11:22:10 +0300

    1

--- task_1.py
+++ task_1.py
@@ -13,3 +13,24 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+from memory_profiler import profile
+from random import randint
+
+@profile
+def search_in_list(search_el):
+    lst2 = [randint(0, i) for i in range(100000)]
+    result = []
+    result2 = []
+    for i in lst2:
+        if i == search_el:
+            result.append(i)
+    lst1 = len(result) * lst2
+    for j in lst1:
+        if i == search_el:
+            result2.append(i)
+    del lst2
+    return len(result), len(result2)
+
+
+print(search_in_list(25))
+
