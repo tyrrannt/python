cfa1d8a066f9d50174dd8fe453777847628eef5a
Shakirov Vitaliy <shakirov.vitaliy@gmail.com>
2020-08-20 15:16:31 +0300

    1 2 3

--- task_1.py
+++ task_1.py
@@ -12,3 +12,29 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+
+
+def generate_list(a, b, n):
+    from random import randint
+    random_list = []
+    for i in range(n):
+        random_list.append(randint(a, b))
+    return random_list
+
+
+def sort_bubble(lst_obj):
+    print(f'Исходный: {lst_obj}')
+    for i in range(len(lst_obj) - 1):
+        for j in range(len(lst_obj) - i - 1):
+            if lst_obj[j] < lst_obj[j + 1]:
+                buff = lst_obj[j]
+                lst_obj[j] = lst_obj[j + 1]
+                lst_obj[j + 1] = buff
+    print(f'Отсортированный: {lst_obj}\n')
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+
+    test1 = Timer(lambda: sort_bubble(generate_list(-100, 100, 50)))
+    print(f'revers = {test1.timeit(10)}')
--- task_3.py
+++ task_3.py
@@ -13,3 +13,33 @@
 arr[m]
 from statistics import median
 """
+from random import randint
+from statistics import median
+
+def gnome_sort(lst, size):
+    i = 1
+    while i < size:
+        if lst[i - 1] <= lst[i]:
+            i += 1
+        else:
+            tmp = lst[i]
+            lst[i] = lst[i - 1]
+            lst[i - 1] = tmp
+            i -= 1
+            if (i == 0):
+                i = 1
+    return lst
+
+def median_2(lst):
+    quotient, remainder = divmod(len(lst), 2)
+    if remainder:
+        return sorted(lst)[quotient]
+    return sum(sorted(lst)[quotient - 1:quotient + 1]) / 2.
+
+m = int(input('Введите m – натуральное число: '))
+lists = [randint(0, 2*m) for i in range(2*m+1)]
+newlists = gnome_sort(lists, len(lists))
+print(lists)
+print(newlists)
+print(median(lists))
+print(median_2(newlists))
