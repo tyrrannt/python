0c919658838e4ad8ba6d6ba2ec6f9800f342628d
Shakirov Vitaliy <shakirov.vitaliy@gmail.com>
2020-11-20 17:36:50 +0300

    1

---
+++ goods.js
@@ -0,0 +1,75 @@
+'use strict';
+const cartItem = {
+    render(product) {
+        return `<div class="product">
+                    <div><b>Наименование</b>: ${product.productName}</div>
+                    <div><b>Цена за шт.</b>: ${product.productPrice}</div>
+                    <div><b>Количество</b>: ${product.productQuantity}</div>
+                    <div><b>Стоимость</b>: ${product.productQuantity * product.productPrice}</div>
+                </div>`;
+    }
+}
+
+const goods = {
+    goodsItemCard: null,
+    cartItem,
+    products: [
+        {
+            productName: 'Apple',
+            productPrice: 120.00,
+            productQuantity: 0
+        },
+        {
+            productName: 'Meat',
+            productPrice: 390.00,
+            productQuantity: 0
+        },
+        {
+            productName: 'Orange',
+            productPrice: 80.00,
+            productQuantity: 0
+        },
+    ],
+    buttonCreate() {
+
+
+    },
+
+    basketFill() {
+        let count;
+        for (let i = 0; i < this.products.length; i++) {
+            count = parseInt(Math.random(100) * 10);
+            this.products[i].productQuantity = count;
+        }
+    },
+
+    getCartPrice() {
+        return this.products.reduce(function (price, good) {
+            return price + good.price * good.quantity;
+        }, 0);
+    },
+
+    basketCost() {
+        return this.products.reduce((totalCost, productItem) => totalCost += (productItem.productPrice * productItem.productQuantity), 0);
+    },
+
+    init() {
+        this.basketFill();
+        this.goodsItemCard = document.querySelector('.products-div');
+        this.render();
+    },
+
+    render() {
+        if (this.products.length) {
+            this.products.forEach(product => {
+                this.goodsItemCard.insertAdjacentHTML('beforeend', this.cartItem.render(product));
+            });
+            this.goodsItemCard.insertAdjacentHTML('beforeend', `В корзине ${this.products.length} позиций(а) стоимостью ${this.basketCost()}`);
+        } else {
+            this.goodsItemCard.textContent = 'Корзина пуста';
+        }
+    },
+
+}
+
+goods.init()
